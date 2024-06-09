bd06fcda061279e84883cd12108b86a8238e63e0
kyledyck@outlook.com <144828203+KyleJamesD@users.noreply.github.com>
2024-06-09 13:14:40 -0600

    finished insertion sort

--- src/shapes/ShapeComparator.java
+++ src/shapes/ShapeComparator.java
@@ -10,60 +10,151 @@ import java.util.Comparator;
  *
  * @author kyled
  */
-public class ShapeComparator implements Comparator<Shape>
+public class ShapeComparator<E extends Shape> implements Comparator<E>
 {
     public int size;
-    public Shape[] data;
+    public E [] data;
+    public String comparetype;
+    public String sortalgo;
 
     public int getSize() {
         return size;
     }
 
-    public Shape[] getData() {
+    public E[] getData() {
         return data;
+    }
+
+    public ShapeComparator(E[] data, String comparetype, String sortalgo) {
+        this.data = data;
+        this.comparetype = comparetype;
+        this.sortalgo = sortalgo;
+        this.size = data.length;
     }
     
     
-            
+     //end constructor and fields      
+     //begin sorting algos
+    
         @Override
-        public int compare(Shape s1, Shape s2)
+        public int compare(E s1, E s2)
         {
-            if (s1.calcBaseArea() > s2.calcBaseArea())
-                {return 1;}
-            
-            else if (s1.calcBaseArea() < s2.calcBaseArea())
-                {return -1;}
-            
-            else
-                {return 0;}
+            return compareShapes(s1, s2, comparetype);
         }
     
+        
+  
+        public int compareShapes(Shape s1, Shape s2, String comparetype)
+        {
+            switch(comparetype.toLowerCase())
+            {
+                case "v":
+                    if (s1.calcVolume() > s2.calcVolume())
+                    {return 1;}
+                    else if (s1.calcVolume() < s2.calcVolume())
+                    {return-1;}
+                    else
+                    {return 0;}
+                case "a":
+                default:
+                    if (s1.calcBaseArea() > s2.calcBaseArea())
+                    {return 1;}
+                    else if (s1.calcBaseArea() < s2.calcBaseArea())
+                    {return -1;}
+                    else
+                    {return 0;}
+                    
+            }   
+            
+        }
+        
+        
+        
+        
     //Correct Implementation of the Bubble sort 
   
-    public void bubblesortArea(Shape[] data)
-    {
-        
-        int size1 = data.length;
-        for (int j=0; j < size1; j++)
+        public void bubblesort(E[] data)
         {
-            for (int y=1; y < size1; y++)
 
+            for (int j=0; j < size; j++)
             {
-                if (compare(data[y], data[y-1]) > 0)
+                for (int y=1; y < size; y++)
+
                 {
-                   Shape x = data[y];
-                   data[y] = data[y-1];
-                   data[y-1] = x;
+                    if (compare(data[y], data[y-1]) > 0)
+                    {
+                       E x = data[y];
+                       data[y] = data[y-1];
+                       data[y-1] = x;
+
+                    }
 
                 }
 
             }
 
-        }
+        }       
 
-    }       
+
+            public void insertionsort(E[] data)
+            {
+
+                for(int i=1; i<size; i++)
+                {
+                        int index = i;
+
+                    for (int j = i-1; j>= 0; j--)
+                    {
+                        if (compare(data[index],data[j]) > 0)
+                        {
+                            E temp = data[index];
+                            data[index] = data[j];
+                            data[j] = temp; 
+                            
+                        }
+                        index = j;
+                    }
+                }
+
+            }
+
 
         
+            // this will take some time to figure out
+            /*
+            public void selectionSort(E[] data)
+            {
+
+                for (int i=0; i<size; i++)
+                {
+                    E min = data[i];
+                    E minindex = i;
+                    
+                    for (int j = i+1; j<size; j++)
+                    {    
+                        if (compare(min , data[j]))
+                        {
+                            min = data[j];
+                            minindex = j;
+                        }
+                    }
+            
+                    swap(i, minindex);
+                }
+            } 
+            
+            
+                    private void swap(int i, int j)
+                {
+                    E temp = data [i];
+                    data[i] = data[j];
+                    data[j] = temp; 
+                }
+            */
+
+
+
+
         
  //end of class
 }
