29ac44a78043c644a5787a5db5a2f1ba9e6cc365
kwin256 <kwin256wei@gmail.com>
2024-06-14 15:49:36 -0600

    merge sort

--- src/utilities/SortContext.java
+++ src/utilities/SortContext.java
@@ -1,17 +1,12 @@
 package utilities;
 
-import shapes.Shape;
 import utilities.algorithm.SortAlgorithm;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.Comparator;
 
 public class SortContext<T> {
 
-    private T[] data;
+    private final T[] data;
 
-    private SortAlgorithm<T> sortAlgorithm;
+    private final SortAlgorithm<T> sortAlgorithm;
 
     public SortContext(SortAlgorithm<T> sortAlgorithm, T[] data) {
 
--- src/utilities/algorithm/MergeSort.java
+++ src/utilities/algorithm/MergeSort.java
@@ -2,7 +2,6 @@ package utilities.algorithm;
 
 import shapes.Shape;
 
-import java.util.Arrays;
 import java.util.Comparator;
 
 public class MergeSort extends SortAlgorithm<Shape> {
@@ -12,7 +11,51 @@ public class MergeSort extends SortAlgorithm<Shape> {
 
     @Override
     public  void sort(Shape[] data) {
-        // todo: implement merge sort
-        Arrays.sort(data, comparator);
+        mergeSort(data, 0, data.length - 1);
+    }
+
+    private void mergeSort(Shape[] data, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = (left + right) / 2;
+        mergeSort(data, left, mid);
+        mergeSort(data, mid + 1, right);
+        merge(data, left, mid, right);
+    }
+
+    private void merge(Shape[] data, int left, int mid, int right) {
+        // temp merge array to store the sorted elements
+        Shape[] temp = new Shape[right - left + 1];
+        // start index of the left subarray
+        int i = left;
+        // start index of the right subarray
+        int j = mid + 1;
+        // start index of the temp array
+        int k = 0;
+        while (i <= mid && j <= right) {
+            if (this.comparator.compare(data[i], data[j]) <= 0) {
+                temp[k] = data[i];
+                k++;
+                i++;
+            } else {
+                temp[k] = data[j];
+                k++;
+                j++;
+            }
+        }
+        while (i <= mid) {
+            temp[k] = data[i];
+            k++;
+            i++;
+        }
+        while (j <= right) {
+            temp[k] = data[j];
+            k++;
+            j++;
+        }
+        for (k = 0; k < temp.length; k++) {
+            data[left + k] = temp[k];
+        }
     }
 }
